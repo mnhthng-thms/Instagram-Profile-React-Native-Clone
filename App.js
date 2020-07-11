0a1fc6ed0ff1c07e37d0979658d16eaa9f2fd6be
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-07-12 06:09:56 +0700

    creating Pages

--- App.js
+++ App.js
@@ -2,44 +2,22 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import colours from './src/styles/colours'
 
-import StatusBar from './src/components/StatusBar'
-import TopNavBar from './src/components/TopNavBar/TopNavBar'
-import Header from './src/components/Header/Header'
-import Counter from './src/components/Counter/Counter'
-import Body from './src/components/Body/Body'
-import BottomNavBar from './src/components/BottomNavBar/BottomNavBar'
+import ProfilePage from './src/pages/ProfilePage'
 
 import { avatar, photos } from './assets/data.json'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    flexDirection: 'column', 
-    backgroundColor: colours["WHITE"]
-  }
-})
 
 const App = function () {
   try {
     return (
-      <View style={styles.container}>
-        <StatusBar/>
-        <TopNavBar/>
-        <Header 
-          avatarPath={avatar}
-          userName="Hoang Minh Tu"
-          occupation="Web Developer"
-        />
-        <Counter
-          photosNum={photos.length}
-          followersNum="15k"
-          followingNum="605"
-        />
-        <Body 
-          data={photos}
-        />
-        <BottomNavBar/>
-      </View>
+      <ProfilePage
+        avatarPath={avatar}
+        username="Hoang Minh Tu"
+        occupation="Web Developer"
+        photosNum={photos.length}
+        followersNum="15k"
+        followingNum="605"
+        fetchPhotosFrom={photos} 
+      />
     )
   } catch (err) {
     console.error(err)
---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import colours from '../styles/colours'
+
+import StatusBar from '../components/StatusBar'
+import TopNavBar from '../components/TopNavBar/TopNavBar'
+import Header from '../components/Header/Header'
+import Counter from '../components/Counter/Counter'
+import Body from '../components/Body/Body'
+import BottomNavBar from '../components/BottomNavBar/BottomNavBar'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    flexDirection: 'column', 
+    backgroundColor: colours["WHITE"]
+  }
+})
+
+const ProfilePage = (props) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar/>
+      <TopNavBar/>
+      <Header 
+        avatarPath={props.avatarPath}
+        userName={props.username}
+        occupation={props.occupation}
+      />
+      <Counter
+        photosNum={props.photoNum}
+        followersNum={props.followersNum}
+        followingNum={props.followingNum}
+      />
+      <Body 
+        data={props.fetchPhotosFrom}
+      />
+      <BottomNavBar/>
+    </View>
+  )
+}
+
+export default ProfilePage
